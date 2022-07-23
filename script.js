36daed7112e9ef73df680909dbb89e9007a033a0
Dzianis <paskin.denis.00gmail.com>
2022-07-23 15:24:00 +0200

    added lesson 26

---
+++ script.js
@@ -0,0 +1,14 @@
+const getData = () => fetch('./db.json')
+
+
+const sentData = (url, data) => {
+  return fetch(url, {
+    method: 'POST',
+    body: data,
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  }).then(response => response.json())
+}
+
+sentData('https://jsonplaceholder.typicode.com/posts', JSON.stringify(getData)).then(data => console.log(data))
